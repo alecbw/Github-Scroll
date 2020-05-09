127ff50e592d2e5746282f0da1b1c19cc75fb5bf
alecbw <abarrett.wilsdon@gmail.com>
2020-05-08 20:38:29 -0700

    actually done this time

--- content.js
+++ content.js
@@ -1,12 +1,19 @@
 // btadd.addEventListener('findBestComment')
-console.log('trigg')
+// alert('trigg')
+
+// chrome.browserAction.onClicked.addListener(function(document) {
+//     alert('working?');
+// });
 
 var thumb_list = [];
 var elements = document.getElementsByClassName("emoji mr-1");
+// alert('foobar')
 
 for(var i = 0; i< elements.length; i++){
+
     if (elements[i].getAttribute('alias') === "+1") {
         thumb_list.push(parseInt(elements[i].nextSibling.nodeValue.trim()))
+
     }
 }
 
@@ -14,14 +21,10 @@ var largest_thumb_value = Math.max.apply(null, thumb_list);
 
 for(var i = 0; i< elements.length; i++){
     if (parseInt(elements[i].nextSibling.nodeValue.trim()) == largest_thumb_value && elements[i].getAttribute('alias') === "+1") {
+        elements[i].style.visibility = 'visible';
+        elements[i].style.display = 'block';
+        elements[i].tabIndex = "-1";
         elements[i].focus();
+        break;
     }
 }
-
-console.log('all done and also why are you reading this')
-
-
-    // "browser_action": {
-    //     "default_title": "Click this button if you want, or don't, I'm not your boss",
-    //     "default_popup": "popup.html"
-    // },
